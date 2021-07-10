26b7d2673649baba32a76d4651bd59bc5857d6b2
ankitcodes001 <64502877+ankitcodes001@users.noreply.github.com>
2021-07-10 06:56:20 +0530

    Add files via upload

---
+++ node_rest_apis/models/User.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+        username: {
+            type: String,
+            require: true,
+            min: 5,
+            max: 20,
+            unique: true,
+        },
+
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            max: 50,
+
+
+
+
+        },
+
+        password: {
+            type: String,
+            require: true,
+            unique: true,
+            min: 6,
+            max: 10,
+
+        },
+
+        profilePicture: {
+            type: String,
+
+            default: "",
+
+
+
+        },
+
+        coverPicture: {
+            type: String,
+            default: "",
+        },
+
+        follower: {
+            type: Array,
+            default: [],
+
+
+        },
+        following: {
+            type: Array,
+            default: [],
+
+
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        desc: {
+            type: String,
+            max: 50,
+
+        },
+        city: {
+            type: String,
+            max: 20,
+        },
+        from: {
+            type: String,
+            max: 20,
+        },
+        relationship: {
+            type: Number,
+            enum:[1,2,3,4],
+        },
+
+
+    }, {
+        timeStamps: true
+    }
+
+);
+
+
+
+module.exports = mongoose.model("User", UserSchema)
